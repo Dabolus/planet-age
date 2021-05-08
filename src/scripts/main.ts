e43040edfcd0319b9f79cf2f3e5a4d53397e2c84
Giorgio Garasto <giorgio@garasto.me>
2021-05-08 20:36:47 +0200

    feat: reimplement using scroll snap

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,95 @@
-import data from '../data/planets.yml';
+import planetsConfig from '../data/planets.yml';
+import { isDate } from './validators';
+import { configurePlanet } from './planet';
+import { navigate, setupRouter, computeUrl, getRoutingData } from './routing';
+import { debounce } from './utils';
 
-console.log(data);
+const birthdayPicker = document.querySelector<HTMLInputElement>('#birthday')!;
+const startButton = document.querySelector<HTMLButtonElement>('#start')!;
+const main = document.querySelector<HTMLDivElement>('main')!;
+
+const { revolutionTime: earthRevolutionTime } = planetsConfig.find(
+  ({ id }) => id === 'earth',
+)!;
+
+const planets = planetsConfig.map(planetConfig =>
+  configurePlanet({
+    ...planetConfig,
+    earthRevolutionTime,
+  }),
+);
+
+const planetsKeyVal = Object.fromEntries(
+  planets.map(planet => [planet.id, planet]),
+);
+
+setupRouter((date, planet) => {
+  if (date) {
+    birthdayPicker.value = date.toISOString().slice(0, 10);
+  }
+  startButton.disabled = !date;
+  main.className = date ? '' : 'locked';
+
+  if (!date || !planet) {
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: 'smooth',
+    });
+    return;
+  }
+
+  const birthday = new Date(date);
+
+  planets.forEach(planet => planet.computeAge(birthday));
+
+  const { ref, background } = planetsKeyVal[planet];
+
+  console.log(ref);
+  ref.scrollIntoView({ behavior: 'smooth' });
+  main.style.backgroundColor = background;
+});
+
+birthdayPicker.addEventListener('input', ({ target }) => {
+  const { value } = target as HTMLInputElement;
+
+  startButton.disabled = !isDate(value);
+});
+
+startButton.addEventListener('click', () => {
+  const { value } = birthdayPicker;
+
+  if (!isDate(value)) {
+    return;
+  }
+
+  navigate(new Date(value), 'earth');
+});
+
+main.addEventListener(
+  'scroll',
+  () => {
+    const planet = planets[Math.round(main.scrollTop / window.innerHeight) - 1];
+
+    if (planet) {
+      main.style.backgroundColor = planet.background;
+    }
+  },
+  { passive: true },
+);
+main.addEventListener(
+  'scroll',
+  debounce(() => {
+    const planet = planets[Math.round(main.scrollTop / window.innerHeight) - 1];
+
+    if (planet) {
+      const { path, date } = getRoutingData();
+      const newPath = computeUrl(date, planet.id);
+
+      if (path !== newPath) {
+        history.pushState({}, '', newPath);
+      }
+    }
+  }, 100),
+  { passive: true },
+);
---
+++ src/scripts/planet.ts
@@ -0,0 +1,82 @@
+import { Planet } from '../data/planets.yml';
+
+export interface PlanetConfiguration extends Planet {
+  readonly earthRevolutionTime: number;
+}
+
+const dateFormat = new Intl.DateTimeFormat();
+
+export interface PlanetInstance extends Planet {
+  readonly ref: HTMLElement;
+  readonly computeAge: (birthday: Date) => void;
+}
+
+export const configurePlanet = ({
+  id,
+  revolutionTime,
+  earthRevolutionTime,
+  ...rest
+}: PlanetConfiguration): PlanetInstance => {
+  const ref = document.querySelector<HTMLElement>(`#${id}`)!;
+  const ageElement = document.querySelector<HTMLElement>(`#${id}-age`)!;
+  const nextBirthdayElement = document.querySelector<HTMLElement>(
+    `#${id}-birthday`,
+  )!;
+
+  return {
+    id,
+    revolutionTime,
+    ref,
+    ...rest,
+    computeAge(birthday: Date) {
+      const now = Date.now();
+      const earthAge = (now - birthday.getTime()) / 86400000; // Number of milliseconds in a day
+
+      const floatYears = earthAge / revolutionTime;
+      const intYears = Math.floor(floatYears);
+      const days = Math.floor((floatYears - intYears) * revolutionTime);
+
+      const yearsString =
+        intYears > 0 ? `${intYears} year${intYears === 1 ? '' : 's'}` : '';
+      const daysString = days > 0 ? `${days} day${days === 1 ? '' : 's'}` : '';
+      const finalString = `≈ ${yearsString}${
+        yearsString && daysString ? ' and' : ''
+      } ${daysString}`;
+
+      ageElement.innerText = finalString;
+
+      if (id === 'earth') {
+        nextBirthdayElement.innerText = `on ${dateFormat.format(birthday)}`;
+
+        return;
+      }
+
+      const nextBirthdayMilliseconds =
+        now +
+        revolutionTime *
+          (1 -
+            (earthAge / revolutionTime > 1
+              ? earthAge / revolutionTime -
+                Math.floor(earthAge / revolutionTime)
+              : earthAge / revolutionTime)) *
+          86400000;
+
+      const nextBirthday = new Date(nextBirthdayMilliseconds);
+
+      // If the date is too large
+      if (isNaN(nextBirthday.valueOf())) {
+        const nextBirthdayYears = Math.floor(
+          nextBirthdayMilliseconds / 86400000 / earthRevolutionTime,
+        );
+
+        nextBirthdayElement.innerText = `≈ ${nextBirthdayYears} years from now`;
+
+        return;
+      }
+
+      const formattedDate = dateFormat.format(nextBirthday);
+
+      nextBirthdayElement.innerText = `on ${formattedDate}`;
+    },
+  };
+};
